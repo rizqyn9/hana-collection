390201c3b3875fa666ac830dd54820924b19effb
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2021-12-21 11:27:06 +0700

    init: tailwindcss jit

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='h-[100vh] bg-green-100 pt-[10rem]'>
+      <h1>This is home page.</h1>
+    </div>
+  )
+}
+
+export default Home
